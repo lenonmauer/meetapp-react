b77ec92987b07ca96529bdbba965b7ea62f88ba6
Lenon <lenonmauer@gmail.com>
2019-03-13 22:35:50 -0300

    increased form height

--- src/pages/SignUp/styles.js
+++ src/pages/SignUp/styles.js
@@ -11,7 +11,7 @@ export const Container = styled.div`
 export const Form = styled.form`
   display: flex;
   justify-content: space-between;
-  height: 380px;
+  height: 420px;
   width: 270px;
   flex-direction: column;
 `;
