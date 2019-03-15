5b07f6103d3bdc12527f805b7d9cfd43498be7d3
Lenon <lenonmauer@gmail.com>
2019-03-15 15:49:14 -0300

    added prop when

--- src/components/ValidationError/index.js
+++ src/components/ValidationError/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import { Container } from './styles';
 
-const ValidationError = ({ children }) => !!children && <Container>{children}</Container>;
+const ValidationError = ({ when, message }) => when && <Container>{message}</Container>;
+
+ValidationError.propTypes = {
+  when: PropTypes.bool.isRequired,
+  message: PropTypes.string,
+};
 
 export default ValidationError;
