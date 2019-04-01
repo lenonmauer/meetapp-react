f41eee1edaa0af6418290210b456821622d27941
Lenon Mauer <lenonmauer@gmail.com>
2019-04-01 14:44:00 -0300

    removed new meetup form

--- src/pages/NewMeetup/index.js
+++ src/pages/NewMeetup/index.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
-import { Formik } from 'formik';
+import { withFormik } from 'formik';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
+import { bindActionCreators, compose } from 'redux';
 
 import { CategoriesActions } from '../../store/ducks/categories';
 import { MeetupActions } from '../../store/ducks/meetup';
+import { UploadActions } from '../../store/ducks/upload';
 
-import { Spinner } from '../../components';
+import Upload from './components/Upload';
 
-import NewMeetupForm from './components/NewMeetupForm';
+import {
+  Input, InputLabel, Button, Checkbox, ValidationError, Spinner,
+} from '../../components';
+
+import { Container, Date, FormGroup } from './styles';
+
 import validationSchema from './validationSchema';
 
 class NewMeetup extends Component {
@@ -19,47 +25,150 @@ class NewMeetup extends Component {
     }
   }
 
+  componentDidUpdate(prevProps) {
+    if (this.props.values.photo === null && prevProps.values.photo !== null) {
+      this.props.resetUpload();
+    }
+  }
+
+  componentWillUnmount() {
+    this.props.resetUpload();
+  }
+
+  handleCheckboxChange = (value) => {
+    const {
+      setFieldValue,
+      values: { categories: options },
+    } = this.props;
+    const field = 'categories';
+
+    if (options.find(option => option === value)) {
+      setFieldValue(field, options.filter(cat => cat !== value));
+    }
+    else {
+      setFieldValue(field, [...options, value]);
+    }
+  };
+
+  handleFileChange = (file) => {
+    const { setFieldValue } = this.props;
+    /* eslint-disable-next-line */
+    setFieldValue('photo', file ? file._id : '');
+  };
+
   render() {
-    const { loadingCategories, categories, postMeetupRequest } = this.props;
+    const {
+      loadingCategories,
+      categories,
+      handleSubmit,
+      values,
+      handleChange,
+      errors,
+    } = this.props;
 
     if (loadingCategories) {
       return <Spinner marginTop="30px" />;
     }
 
     return (
-      <Formik
-        initialValues={{
-          title: '',
-          description: '',
-          date: '',
-          localization: '',
-          photo: null,
-          categories: [],
-        }}
-        validationSchema={validationSchema}
-        validateOnBlur={false}
-        validateOnChange={false}
-        onSubmit={(values, { resetForm }) => postMeetupRequest(values, resetForm)}
-        render={formikProps => (
-          <NewMeetupForm
-            {...formikProps}
-            categories={categories}
-          />
-        )}
-      />
+      <Container>
+        <form onSubmit={handleSubmit} autoComplete="off">
+          <FormGroup>
+            <InputLabel>Título</InputLabel>
+            <Input
+              name="title"
+              value={values.title}
+              onChange={handleChange}
+              placeholder="Digite o título do meetup"
+            />
+            <ValidationError when={!!errors.title} message={errors.title} />
+          </FormGroup>
 
+          <FormGroup>
+            <InputLabel>Descrição</InputLabel>
+            <Input
+              name="description"
+              value={values.description}
+              onChange={handleChange}
+              placeholder="Descreva seu meetup"
+            />
+            <ValidationError when={!!errors.description} message={errors.description} />
+          </FormGroup>
+
+          <FormGroup>
+            <InputLabel>Data e hora do início</InputLabel>
+            <Date
+              name="date"
+              mask="11/11/1111 11:11"
+              value={values.date}
+              onChange={handleChange}
+              placeholder="informe a data de início do meetup"
+            />
+            <ValidationError when={!!errors.date} message={errors.date} />
+          </FormGroup>
+
+          <FormGroup>
+            <InputLabel>Imagem</InputLabel>
+            <Upload onFileChange={this.handleFileChange} />
+            <ValidationError when={!!errors.photo} message={errors.photo} />
+          </FormGroup>
+
+          <FormGroup>
+            <InputLabel>Localização</InputLabel>
+            <Input
+              name="localization"
+              value={values.localization}
+              onChange={handleChange}
+              placeholder="Onde seu meetup vai acontecer?"
+            />
+            <ValidationError when={!!errors.localization} message={errors.localization} />
+          </FormGroup>
+
+          <FormGroup>
+            <InputLabel>Tema do meetup</InputLabel>
+            {categories.map(category => (
+              <Checkbox
+                key={category.id}
+                id={`category-${category.id}`}
+                value={category.id}
+                checked={!!values.categories.find(cat => cat === category.id)}
+                onChange={() => this.handleCheckboxChange(category.id)}
+              >
+                {category.name}
+              </Checkbox>
+            ))}
+            <ValidationError when={!!errors.categories} message={errors.categories} />
+          </FormGroup>
+
+          <Button>Salvar</Button>
+        </form>
+      </Container>
     );
   }
 }
 
 NewMeetup.propTypes = {
   getCategoriesRequest: PropTypes.func.isRequired,
-  postMeetupRequest: PropTypes.func.isRequired,
-  categories: PropTypes.arrayOf(PropTypes.shape({
-    id: PropTypes.string,
-    name: PropTypes.string,
-  })).isRequired,
+  categories: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string,
+      name: PropTypes.string,
+    }),
+  ).isRequired,
   loadingCategories: PropTypes.bool.isRequired,
+  resetUpload: PropTypes.func.isRequired,
+  setFieldValue: PropTypes.func.isRequired,
+  handleSubmit: PropTypes.func.isRequired,
+  handleChange: PropTypes.func.isRequired,
+  values: PropTypes.shape({
+    title: PropTypes.string,
+    description: PropTypes.string,
+    date: PropTypes.string,
+    localization: PropTypes.string,
+    photo: PropTypes.string,
+    categories: PropTypes.arrayOf(PropTypes.string),
+  }).isRequired,
+  errors: PropTypes.shape().isRequired,
 };
 
 const mapStateToProps = state => ({
@@ -68,9 +177,35 @@ const mapStateToProps = state => ({
   loadingCategories: state.categories.loading,
 });
 
-const mapDispatchToProps = dispatch => bindActionCreators({
-  ...CategoriesActions,
-  ...MeetupActions,
-}, dispatch);
+const mapDispatchToProps = dispatch => bindActionCreators(
+  {
+    ...CategoriesActions,
+    ...MeetupActions,
+    ...UploadActions,
+  },
+  dispatch,
+);
 
-export default connect(mapStateToProps, mapDispatchToProps)(NewMeetup);
+export default compose(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps,
+  ),
+  withFormik({
+    mapPropsToValues: ({ values }) => values || {
+      title: '',
+      description: '',
+      date: '',
+      localization: '',
+      photo: null,
+      categories: [],
+    },
+
+    validationSchema,
+
+    validateOnChange: false,
+    validateOnBlur: false,
+
+    handleSubmit: (values, { props }) => props.postMeetupRequest(values, props.resetForm),
+  }),
+)(NewMeetup);
